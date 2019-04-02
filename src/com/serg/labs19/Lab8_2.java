dada13db93c2affde7ad23ab2f4bb66a606fb894
serg <serg@CatXer.168.0.10>
2019-04-03 00:24:22 +0300

    Fix 8_2

--- src/com/serg/labs19/Lab8_2.java
+++ src/com/serg/labs19/Lab8_2.java
@@ -6,28 +6,36 @@ public class Lab8_2 {
 	public static void main(String[] args) {
 		// init//
 		Scanner in = new Scanner(System.in);
-		String[] words;
-		String result = "";
-		int k;
+		String startStr, word = "";
+		StringBuilder result = new StringBuilder();
+		int k, pos = 0;
 		// input//
-		System.out.println("Enter any stroke:");
-		words = in.nextLine().split(" ");
-		System.out.println("Enter [K] position to revert [K] word:");
+		System.out.println("Enter any stroke ->");
+		startStr = in.nextLine() + " ";
+		System.out.print("Enter [K] position to revert [K] word:");
 		k = Math.abs(in.nextInt());
 		in.close();
 		// refactor stroke//
-		boolean revert = false;
-		for (int i = 0; i < words.length; i++) {
-			if (!revert && (i + 1) % k == 0)
-				revert = true;
-			if (revert && words[i].matches(".*[\\w\\p{InCyrillic}].*")) {
-				String reverse = "";
-				for (int j = words[i].length() - 1; j >= 0; j--)
-					reverse += words[i].charAt(j);
-				words[i] = reverse;
-				revert = false;
-			}
-			result += " " + words[i];
+		for (int i = 0; i < startStr.length(); i++) {
+			char c = startStr.charAt(i);
+			// 65 - 122
+			// 1040 - 1103
+			// 48 - 57
+
+			// if (("" + c).matches(".*[\\w\\p{InCyrillic}].*")) {
+			if (65 <= c && c <= 122 || 1040 <= c && c <= 1103 || 48 <= c && c <= 57) {
+				word += c;
+			} else if (word.length() > 0) {
+				if (++pos == k) {
+					pos = 0;
+					for (int j = word.length() - 1; j >= 0; j--)
+						result.append(word.charAt(j));
+				} else
+					result.append(word);
+				word = "";
+				result.append(c);
+			} else
+				result.append(c);
 		}
 		// print result//
 		System.out.println("New stroke:\n" + result);
